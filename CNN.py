a0269c4456ea2abb112bb4398eb870223920915d
Vasudha299 <vasudhashrivastava29@gmail.com>
2024-06-30 00:12:24 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -1,3 +1,6 @@
+""" Design of a small CNN using Pytorch for MNIST handwritten digit image classification.
+Trained it on the training set for 50 epochs and reporting the accuracy on the test set after 10, 20, 30, 40, and 50 epochs.
+"""
 import torch
 import torch.nn as nn
 import torch.optim as optim
